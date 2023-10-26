37d17bb425525974a7e606fec3773fc128edccec
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2023-10-26 15:35:30 -0300

    Login page designed

---
+++ Login-Page/src/app/sign-page/sign-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-page',
+  templateUrl: './sign-page.component.html',
+  styleUrls: ['./sign-page.component.scss']
+})
+export class SignPageComponent {
+
+}
